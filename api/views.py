5f990ede350c7ada58fbd03933ec2c4328db3083
Toluwani <abiodun.toluwanii@gmail.com>
2018-05-31 12:40:05 +0100

    'fgfg'

--- api/views.py
+++ api/views.py
@@ -20,7 +20,7 @@ def LoginView(request):
     try:
         login_data = json.loads(payload)
 
-    except TypeError:
+    except:
         response['message'] = 'Type error'
         #the dat is not in not in json
     username=login_data.get('username')
@@ -32,20 +32,9 @@ def LoginView(request):
         btc = get_wallet('btc', user)
         manna = get_wallet('MAN', user)
         #data = serializers.serialize('json', user, fields=('first_name', 'last_name', 'username', 'id'))
-        data={'username':user.username, 'first_name':user.first_name, 'last_name':user.first_name, 'id':user.pk,
-              'wallets': [{'eth_balance':eth.main_balance,
-                          'coin_symbol':eth.coin,
-                          'id':eth.id,
-                          'name':'Ethereum' },
-                         {'btc_balance': btc.main_balance,
-                          'coin_symbol': btc.coin,
-                          'id': btc.id,
-                          'name': 'Bitcoin'},
-                         {'manna_balance': btc.main_balance,
-                          'coin_symbol': btc.coin,
-                          'id': btc.id,
-                          'name': 'Manna'}
-                         ]}
+        data={'username':user.username, 'first_name':user.first_name,
+              'last_name':user.first_name, 'id':user.pk,'email':user.email}
+
         response['status']=200
         response['message']='Successful Login'
         response['data']=data
@@ -64,3 +53,18 @@ def get_wallet(wallet, user):
     wallet = Wallet.objects.get(Q(owner=user), coin=wallet)
     return wallet
 
+'''
+ 'wallets': [{'eth_balance':eth.main_balance,
+                          'coin_symbol':eth.coin,
+                          'id':eth.id,
+                          'name':'Ethereum' },
+                         {'btc_balance': btc.main_balance,
+                          'coin_symbol': btc.coin,
+                          'id': btc.id,
+                          'name': 'Bitcoin'},
+                         {'manna_balance': btc.main_balance,
+                          'coin_symbol': btc.coin,
+                          'id': btc.id,
+                          'name': 'Manna'}
+                         ]}
+'''

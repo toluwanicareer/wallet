4ce31a3531e1cb5aea616e9b38903dd91afff955
Toluwani <abiodun.toluwanii@gmail.com>
2018-05-31 23:21:06 +0100

    'fgfg'

--- api/models.py
+++ api/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-from rest_framework import serializers
+#from rest_framework import serializers
 
 class Rate(models.Model):
     eth=models.DecimalField(decimal_places=10, max_digits=30)
